ad21b1ddbcaf2b98286941781756954320395c4a
Louis Nwamadi <louisdante9@gmial.com>
2021-02-25 15:46:20 +0100

    connect to database

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,6 @@
-// module.exports = {
-//     dbUrl: process.env.dbUrl
-// }
+import dotenv from 'dotenv'
+dotenv.config()
+
+module.exports = {
+  dbUrl: process.env.DB_URL
+}
--- src/server.js
+++ src/server.js
@@ -1,28 +1,32 @@
-import express, { json, urlencoded } from 'express'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import cors from 'cors'
-import { globalErrorHandler } from './utilities/errors/globalErrorHandler';
-import { routes } from './routes';
+import express, { json, urlencoded } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import { globalErrorHandler } from "./utilities/errors/globalErrorHandler";
+import { routes } from "./routes";
+import db from "./config";
+import dbconnect from "./config/dbcnx";
 dotenv.config();
+
 const app = express();
-const port = parseInt(process.env.PORT) || 3000;
+const port = parseInt(process.env.PORT) || 8000;
 
-app.use(
-  json(),
-  urlencoded({ extended: false }),
-  morgan('dev'),
-  cors()
-);
+app.use(json(), urlencoded({ extended: false }), morgan("dev"), cors());
 
-app.use('/api/v1', routes(express));
-app.get('/', (req, res) => res.send('welcome to restaurants and ratings api'))
-app.use(globalErrorHandler);
-app.get('*', (req, res) => res.send({ err: 'route not found' }))
-
-app.listen(port, (err) => {
-  if (err) {
-    throw new Error(err.message)
-  }
-  console.log(`Server is running on port ${port}`);
-});
+dbconnect(db)
+  .then(() => {
+    app.use("/api/v1", routes(express));
+    app.get("/", (req, res) =>
+      res.send("welcome to restaurants and ratings api")
+    );
+    app.use(globalErrorHandler);
+    app.get("*", (req, res) => res.send({ err: "route not found" }));
+    console.log("Database connected on port 27017");
+    app.listen(port, (err) => {
+      if (err) {
+        throw new Error(err.message);
+      }
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => err);
